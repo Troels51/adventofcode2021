488f3886421dccd912922da1bc88ac7d8d8d935f
Troels Dalsgaard Hoffmeyer <Troels.d.hoffmeyer@gmail.com>
2021-12-16 19:54:56 +0100

    Solve day 6

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use serde::{de::Error, Deserialize, Deserializer}; // 1.0.94
 use bmp::{Image, Pixel};
 
 use crate::{day2::{Submarine, Command}, binary_diagnostics::diagnostic};
+use itertools::iterate;
 mod day1;
 pub mod day2;
 pub mod binary_diagnostics;
@@ -17,22 +18,38 @@ pub mod day4_data;
 pub mod hydrothermal;
 pub mod day5_data;
 
-fn main() {
-    let mut img = Image::new(1000, 1000);
-    let data:Vec<hydrothermal::Line> = day5_data::day5data();
-    let mut map: hydrothermal::Map = hydrothermal::Map{overlappings: vec![vec![0;1000];1000] };
-    for line in data {
-        if line.into_iter().count() == 0{
-        }
-        for p in line {
-            img.set_pixel(p.x as u32, p.y as u32, px!(255, 255, 255));
-        }
-        map.draw_points_that_cover_line(line);
+fn lantern_fish_day(fish: &Vec<u64>) -> Vec<u64> {
+    let spawning_fish = fish[0];
+    let mut new_population = fish[1..9].to_vec();
+
+    new_population[6] += spawning_fish;
+    new_population.push(spawning_fish);
+    new_population
+}
+fn fish_counting(fish_population: Vec<u64>) -> Vec<u64>{
+    let mut fish_counting = vec![0;10];
+
+    for fish in fish_population {
+        fish_counting[fish as usize] += 1;
     }
-    let result = map.count_above(2);
-
-    
-    let _ = img.save("img.bmp");
-    println!("Result is : {}", result);
-
+    fish_counting
+}
+fn main() {
+    let fish_population = fish_population_dataset();
+    // The count of number of fish with that index counter
+    let lantern_fish :Vec<u64> = fish_counting(fish_population);
+    let result = iterate(lantern_fish, |fish| lantern_fish_day(fish)).nth(256);
+    let sum : u64 = result.unwrap().into_iter().sum();
+    dbg!(sum);
+}
+#[test]
+fn example_fish() {
+    // The count of number of fish with that index counter
+    let lantern_fish :Vec<u64> = vec![0,1,1,2,1,0,0,0,0,0];
+    let result = iterate(lantern_fish, |fish| lantern_fish_day(fish)).nth(80);
+    let sum : u64 = result.unwrap().into_iter().sum();
+    assert_eq!(sum, 5934)
+}
+fn fish_population_dataset() -> Vec<u64> {
+    vec![1,1,1,2,1,1,2,1,1,1,5,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,4,1,1,1,1,3,1,1,3,1,1,1,4,1,5,1,3,1,1,1,1,1,5,1,1,1,1,1,5,5,2,5,1,1,2,1,1,1,1,3,4,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,5,4,1,1,1,1,1,5,1,2,4,1,1,1,1,1,3,3,2,1,1,4,1,1,5,5,1,1,1,1,1,2,5,1,4,1,1,1,1,1,1,2,1,1,5,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,3,1,1,3,1,3,1,4,1,5,4,1,1,2,1,1,5,1,1,1,1,1,5,1,1,1,1,1,1,1,1,1,4,1,1,4,1,1,1,1,1,1,1,5,4,1,2,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,4,1,1,1,2,1,4,1,1,1,1,1,1,1,1,1,4,2,1,2,1,1,4,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,3,2,1,4,1,5,1,1,1,4,5,1,1,1,1,1,1,5,1,1,5,1,2,1,1,2,4,1,1,2,1,5,5,3]
 }
